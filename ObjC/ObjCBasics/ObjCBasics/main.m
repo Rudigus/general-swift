34a417cb962f74ccdeb7b5eefce921b6eb86eacf
Rudigus <rodrigo.matos9@hotmail.com>
2021-07-20 10:26:55 -0300

    adds objc basics initial project files

---
+++ ObjC/ObjCBasics/ObjCBasics/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ObjCBasics
+//
+//  Created by Rudigus on 20/07/21.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
